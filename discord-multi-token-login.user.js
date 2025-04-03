fc8a1696a02b0a8e9e4323d8a27f1abc17576d55
Nanachi <a@techz.jp>
2025-04-03 11:01:28 +0900

    Update discord-multi-token-login.user.js

--- discord-multi-token-login.user.js
+++ discord-multi-token-login.user.js
@@ -48,7 +48,7 @@
             </div>
             <button id="toggleButton" style="width: 100%; margin-bottom: 10px; padding: 10px; background-color: #575757; color: #ffffff; border: none; border-radius: 3px; cursor: pointer; transition: background-color 0.3s;">トークンログイン</button>
             <div id="content" style="display: none;">
-                <h2 style="margin: 0 0 10px 0;"AARR Multi Token Login V6.3</h2>
+                <h2 style="margin: 0 0 10px 0;">AARR Multi Token Login V6.3</h2>
                 <a href="https://aarr-homepage.github.io/page/about5.html" target="_blank" style="color: #00BFFF; text-decoration: underline; display: block; margin-bottom: 10px;">🔧その他のツール</a>
                 <a href="https://greasyfork.org/ja/scripts/523829-manage-multi-discord-accounts/code" target="_blank" style="color: #00BFFF; text-decoration: underline; display: block; margin-bottom: 10px;">⚙️ソースコード</a>
                 <div id="groupButtons" style="display: flex; justify-content: space-between; margin-bottom: 10px;">
